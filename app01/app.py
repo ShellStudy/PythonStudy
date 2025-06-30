e0b8cfe47a4e0fb628b1caded38c8cdab0cdf7ef
JosephBean <kk1593@naver.com>
2025-06-30 10:53:25 +0900

    파일 쓰기 알아보기

--- app01/app.py
+++ app01/app.py
@@ -1,5 +1,8 @@
 print("안녕하세요")
 
-file = open('README.md', encoding='UTF-8')
-for row in file.readlines():
-    print(row)
+file = open('app01\\text.txt', encoding='UTF-8', mode='w')
+file.write('안녕하세요')
+
+file.close()
+file = open('app01\\text.txt', encoding='UTF-8', mode='r')
+print(file.readlines())
