caa98733572bfccf735b43aad4542c3b429c8894
JosephBean <kk1593@naver.com>
2025-06-30 15:07:29 +0900

    메모장 프로그램 만들기

--- app01/memo.py
+++ app01/memo.py
@@ -13,4 +13,22 @@ def 메모장글쓰기(파일):
         
     파일.close()
     
-메모장글쓰기( 메모장생성() )
+#메모장글쓰기( 메모장생성() )
+    
+def 메모장글열기():
+    파일명 = input("파일명을 입력하세요.")
+    return open(f'app01\\{파일명}', mode='r', encoding='UTF-8')
+    
+def 메모장글출력(파일):
+    for row in 파일.readlines():
+        row = row.replace("\n", "")
+        print(row)
+    파일.close()
+   
+#메모장글출력( 메모장글열기() )
+
+def 메모장글추가():
+    파일명 = input("파일명을 입력하세요.")
+    return open(f'app01\\{파일명}', mode='a', encoding='UTF-8')
+
+#메모장글쓰기( 메모장글추가() )
