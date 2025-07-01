dfbb43be7b4ab6ef13e6179437546a1bb23d864e
JosephBean <kk1593@naver.com>
2025-07-01 12:02:38 +0900

    컬럼명과 데이터 출력하기

--- app02/app.py
+++ app02/app.py
@@ -15,12 +15,18 @@ sql = 'SELECT  * FROM `edu`.`NOTICE` WHERE NO = 1'
 cur.execute(sql)
 result = cur.fetchone()
 
+col_name = cur.description
+name = ""
+for row in col_name:
+    name += row[0] + ("\t\t\t" if row[0] == 'regDate' else "\t")
+print(name)
+
 if result == None:
     print("데이터가 없습니다.")
 else:
     행 = ""
-    print('no\ttitle\tdesc\tcontent\tregDate\t\t\tmodDate')
-    for col in result:
+    # print('no\ttitle\tdesc\tcontent\tregDate\t\t\tmodDate')
+    for col in result:        
         if col == None:
             행 += "없다\t"
         else:
