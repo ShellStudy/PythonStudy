d25ec045e66ec85622afedb9b383f2875add5819
JosephBean <kk1593@naver.com>
2025-07-02 10:38:04 +0900

    데이터 추가 알아보기

--- app03/app.py
+++ app03/app.py
@@ -9,7 +9,9 @@ conn_params = {
 conn = mariadb.connect(**conn_params)
 cur = conn.cursor()
 
-
+sql = "INSERT INTO NOTICE (`title`) VALUE ('추가2 python')"
+cur.execute(sql)
+conn.commit()
 
 cur.close()
 conn.close()
