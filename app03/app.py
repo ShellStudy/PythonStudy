66a0378810570a95cd50f904b4fef25107d8b7e1
JosephBean <kk1593@naver.com>
2025-07-02 14:31:44 +0900

    데이터 읽기 (SELECT) 기능 추가하기

--- app03/app.py
+++ app03/app.py
@@ -1,4 +1,5 @@
 import mariadb
+
 def 마리아디비():
     conn_params = {
         "user" : "study",
@@ -23,9 +24,36 @@ def 입력():
     cur.close()
     conn.close()
 
+def 읽기():
+    conn = 마리아디비()
+    cur = conn.cursor()
+    
+    sql = 'SELECT  * FROM `edu`.`NOTICE`'
+    cur.execute(sql)
+    result = cur.fetchall()
+    
+    if result == None:
+        print("데이터가 없습니다.")
+    else:
+        for row in result:
+            행 = ""
+            for col in row:
+                if col == None:
+                    행 += "\t"
+                else:
+                    행 += f'{col} '
+            print(행)
+    
+    cur.close()
+    conn.close()
+
 while True:
     모드 = input("CRUD 중 선택하세요.")
+    print("="*100)
     if 모드 == 'C':
         입력()
+    elif 모드 == 'R':
+        읽기()
     else:
         break
+    print("="*100)
