9ab221da0fd81d74f0eaa30d8005860b80ddcca6
JosephBean <kk1593@naver.com>
2025-07-08 10:23:59 +0900

    URL 패턴 알아보기

--- app06/main.py
+++ app06/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"test": 1}
+
+@app.get("/test")
+def test(a : int):
+    return {"data": 12345 + a}
+
+@app.post("/body")
+def body(a : int):
+    return {"body": "OK", "data": a}
