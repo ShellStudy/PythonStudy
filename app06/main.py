5afa44c767f3a8366a9c379a728155a7d9d1b71e
JosephBean <kk1593@naver.com>
2025-07-08 10:56:56 +0900

    라우터 알아보기

--- app06/main.py
+++ app06/main.py
@@ -1,15 +1,5 @@
 from fastapi import FastAPI
+from controller import root
 
 app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"test": 1}
-
-@app.get("/test")
-def test(a : int):
-    return {"data": 12345 + a}
-
-@app.post("/body")
-def body(a : int):
-    return {"body": "OK", "data": a}
+app.include_router(root.ctr)
