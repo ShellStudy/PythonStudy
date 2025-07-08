cb6235ead8a95e06f7b77e77a2635836da0f7958
JosephBean <kk1593@naver.com>
2025-07-08 13:58:54 +0900

    country 검색 추가

--- app07/controller/movie.py
+++ app07/controller/movie.py
@@ -21,3 +21,21 @@ def home():
     cur.close()
     conn.close()
     return {"movies": result}
+
+@ctr.get("/country")
+def country(name):
+    conn_params = {
+        "user" : os.getenv('MARIADB_USER'),
+        "password" : os.getenv('MARIADB_PASSWORD'),
+        "host" : os.getenv('MARIADB_HOST'),
+        "database" : os.getenv('MARIADB_DATABASE'),
+        "port" : int(os.getenv('MARIADB_PORT'))
+    }
+    conn = mariadb.connect(**conn_params)
+    cur = conn.cursor()
+    sql = f"SELECT * FROM movies WHERE country LIKE '%{name}%'"
+    cur.execute(sql)
+    result = cur.fetchall()
+    cur.close()
+    conn.close()
+    return {"result": result}
